9272720684014f9964b3f83f9f103ceff4925e0b
rubaqeshta04 <qeshtaruba@gmail.com>
2026-02-23 12:41:14 +0200

    fix env

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ import { createClient } from "@supabase/supabase-js";
 import bcrypt from "bcryptjs";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: "../.env.local" });
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.static("public"));
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
 );
 
 app.post("/api/seller/login", async (req, res) => {
@@ -91,7 +92,6 @@ app.post("/api/products", async (req, res) => {
   const token = authHeader.split(" ")[1];
 
   try {
-
     const { data: seller, error: authError } = await supabase
       .from("sellers")
       .select("id, email, role")
@@ -169,7 +169,6 @@ app.put("/api/products/:id", async (req, res) => {
   const token = authHeader.split(" ")[1];
 
   try {
-
     const { data: seller, error: authError } = await supabase
       .from("sellers")
       .select("id, email, role")
